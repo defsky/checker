3515dfba86eca1c044591b537c9e6385c59ef7aa
def <defsky@qq.com>
2020-11-30 17:19:16 +0800

    添加检查结果汇总报告表

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -63,6 +64,8 @@ func main() {
 	}
 	log.Printf("已加载数据行数：%d", rowCount)
 
+	report := make([]string, 0)
+
 	log.Println("检查重复项 ...")
 	soDoc, err := data.GetColumnByName("销售订单号")
 	if err != nil {
@@ -72,7 +75,9 @@ func main() {
 	hasDump := false
 	for k, v := range dump {
 		hasDump = true
-		log.Printf("发现重复项：%s , 数量: %d", k, v)
+		msg := fmt.Sprintf("发现重复项：%s , 数量: %d", k, v)
+		report = append(report, msg)
+		log.Println(msg)
 	}
 	if hasDump {
 		soDoc = newSoDoc
@@ -109,7 +114,9 @@ func main() {
 	log.Printf("找到源单据条数：%d", srcCount)
 
 	if srcCount != rowCount {
-		log.Printf("找到的单据数和源数据不匹配: db(%d), src(%d)", len(srcDoc), rowCount)
+		msg := fmt.Sprintf("找到的单据数和源数据不匹配: db(%d), src(%d)", len(srcDoc), rowCount)
+		report = append(report, msg)
+		log.Printf(msg)
 	}
 
 	log.Println("搜索未终止的订单 ...")
@@ -119,7 +126,9 @@ func main() {
 		for _, r := range srcDoc {
 			if r["OrderNumber"] == v {
 				if len(r["ClosedBy"]) <= 0 {
-					log.Printf("发现未终止的订单：%s", v)
+					msg := fmt.Sprintf("发现未终止的订单：%s", v)
+					report = append(report, msg)
+					log.Println(msg)
 				}
 				lot := r["TotalNumber"]
 				if len(lot) > 0 {
@@ -153,13 +162,17 @@ func main() {
 				soFound := false
 				for _, row := range lotCodes {
 					if row[1] == srcLotCode {
-						log.Printf("在U9系统中找到总编号: %s, 所属订单号:%s", row[1], row[0])
+						msg := fmt.Sprintf("在U9系统中找到总编号: %s, 所属订单号:%s", row[1], row[0])
+						report = append(report, msg)
+						log.Println(msg)
 						soFound = true
 						break
 					}
 				}
 				if !soFound {
-					log.Printf("在U9系统中找到总编号: %s, 所属订单号:%s", srcLotCode, "<未知>")
+					msg := fmt.Sprintf("在U9系统中找到总编号: %s, 所属订单号:%s", srcLotCode, "<未知>")
+					report = append(report, msg)
+					log.Printf(msg)
 				}
 			}
 		}
@@ -188,7 +201,9 @@ func main() {
 	for _, arRow := range srcAR {
 		m, err := strconv.ParseFloat(arRow["TotalMoney"], 64)
 		if err != nil {
-			log.Printf("应收金额格错误：%s", err)
+			msg := fmt.Sprintf("应收金额格式错误：%s", err)
+			report = append(report, msg)
+			log.Println(msg)
 		} else {
 			totalMoney += m
 		}
@@ -198,19 +213,27 @@ func main() {
 			if strings.Contains(arRow["DocNo"], reqSODoc) {
 				found = true
 				if rm, er := strconv.ParseFloat(reqMoney, 64); err != nil {
-					log.Printf("来源金额格式不正确：%s", er)
+					msg := fmt.Sprintf("来源金额格式不正确：%s", er)
+					report = append(report, msg)
+					log.Printf(msg)
 				} else {
 					if rm != m {
-						log.Printf("定金应收金额与源数据金额不一致：%s\t(%f),\t%s\t(%f)", arRow["DocNo"], m, reqSODoc, rm)
+						msg := fmt.Sprintf("定金应收金额与源数据金额不一致：%s\t(%f),\t%s\t(%f)", arRow["DocNo"], m, reqSODoc, rm)
+						report = append(report, msg)
+						log.Println(msg)
 					}
 				}
 			}
 		}
 		if !found {
-			log.Printf("没有找到应收单对应的来源订单号: %s", arRow["DocNo"])
+			msg := fmt.Sprintf("没有找到应收单对应的来源订单号: %s", arRow["DocNo"])
+			report = append(report, msg)
+			log.Print(msg)
 		}
 	}
-	log.Printf("应收总金额为: %f", totalMoney)
+	msg := fmt.Sprintf("应收总金额为: %f", totalMoney)
+	report = append(report, msg)
+	log.Print(msg)
 
 	log.Println("检查历史归档中是否存在源数据 ...")
 	testdb := db.Mssql("test")
@@ -220,6 +243,9 @@ func main() {
 		log.Fatalf("数据库查询错误：%s", err)
 	}
 	if len(histDoc) > 0 {
+		msg := fmt.Sprintf("发现已归档过的单据 %d 个", len(histDoc))
+		report = append(report, msg)
+
 		log.Printf("发现已归档的单据 %d 个,明细如下：", len(histDoc))
 		for _, row := range histDoc {
 			log.Printf("\t%s\t(%s)", row["DocNo"], row["TotalMoney"])
@@ -271,6 +297,13 @@ func main() {
 		}
 	} else {
 		log.Printf("不需要归档，处理结束")
+	}
+
+	if len(report) > 0 {
+		fmt.Println("\n数据检查报告\n---------------------------------------------------------------------------------------")
+		for i, v := range report {
+			fmt.Printf("\t%d\t%s\n", i+1, v)
+		}
 	}
 }
 
